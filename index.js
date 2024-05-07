15f5e201be8076109ce8fa56c6a87257790d049c
Sualeh Farooq <72302978+sualeh-farooq@users.noreply.github.com>
2024-05-07 14:40:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    console.log("Uploaded on cyclic server , Root")
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
